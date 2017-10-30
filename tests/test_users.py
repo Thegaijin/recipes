605dc8e6b8518a8d7ef84e02bdacdafff14911cf
Thegaijin <semaganda@outlook.com>
2017-10-31 01:31:04 +0300

    Added some tests for testing creating a category functionality

---
+++ tests/test_users.py
@@ -0,0 +1,41 @@
+# app/tests/test_users.py
+
+# third party imports
+import pytest
+
+# local imports
+from app.models.users import User
+from app.models.category import category
+
+
+@pytest.fixture(scope='module')
+def user():
+    ''' Returns an instance of the User class '''
+    return User(1, "username", "password")
+
+
+def test_users_is_instance(user):
+    """Test if instance of User class is successfully created"""
+
+    assert isinstance(user, User) == True
+
+
+def test_users_raise_error_if_argument_not_string(user):
+    ''' Test if the parameter passed to a category is a string '''
+    with pytest.raises(TypeError):
+        user.create_category(2347)
+
+
+def test_users_category_is_created(user):
+    ''' Test if the category is created and added to the list '''
+
+    before_length = len(user.categories)
+    user.create_category("cakes")
+    after_length = len(user.categories)
+    x = after_length - before_length
+    assert x == 1
+
+
+def test_users_category_doesnt_already_exist(user):
+    ''' Test if the category to be created does not already exist '''
+    pass
--- tests/test_yummyapp.py
+++ tests/test_yummyapp.py
@@ -13,16 +13,4 @@ def user_creator():
     return YummyApp()
 
 
-def test_yummyapp_paramater_is_instance_of_user(user_creator):
-    new_user = User(1, 'username', 'password')
-    user_creator.signup(new_user)
-    assert isinstance(new_user, User) == True
-
-
-def test_yummyapp_can_signup_user(user_creator):
-    new_user = User(1, 'username', 'password')
-    assert user_creator.signup(new_user) == True
-
-
-def test_yummyapp_cannot_signup_if_user_already_exists(user_creator):
-    new_user = User(2, 'username', 'password')
+# FIXME:

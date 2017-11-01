1053b7b2ad58dae446160f31d0f32e57fa6e3ed3
Thegaijin <semaganda@outlook.com>
2017-11-01 12:40:14 +0300

    Added functionality to create_category and view_category methods as well as parameters in other methods

--- app/models/users.py
+++ app/models/users.py
@@ -7,36 +7,46 @@ class User(object):
     def __init__(self):
         self.categories = {}
 
-    def create_category(self, name):
+    def create_category(self, category_name):
         ''' Creates the Category 
-        :param: name
+            :param category_name: A string: the name of the category to create
+            :return: The dictionary of categories, with the names as the keys 
+            and an empty list as the values
         '''
-        pass
+        if not isinstance(category_name, str):
+            raise TypeError('Input should be a string')
 
-    def view_category(self, name):
+        if category_name not in self.categories:
+            self.categories[category_name] = []
+            return self.categories
+        return ""
+
+    def view_category(self, category_name):
         ''' Displays a Category '''
+        viewed = {k: v for k, v in self.categories.items()
+                  if category_name in k}
         pass
 
-    def edit_category(self, name, newname):
+    def edit_category(self, category_name, newname):
         ''' Edits a Category '''
         pass
 
-    def delete_category(self, name):
+    def delete_category(self, category_name):
         ''' Deletes a Category '''
         pass
 
-    def create_recipe(self, name):
+    def create_recipe(self, category_name, recipe_name):
         ''' Creates a recipe '''
         pass
 
-    def view_recipe(self, name):
+    def view_recipe(self, category_name, recipe_name):
         ''' Creates a recipe '''
         pass
 
-    def edit_recipe(self, name, newname):
+    def edit_recipe(self, category_name, recipe_name, new_recipe_name):
         ''' Creates a recipe '''
         pass
 
-    def delete_recipe(self, name):
+    def delete_recipe(self, category_name, recipe_name):
         ''' Creates a recipe '''
         pass

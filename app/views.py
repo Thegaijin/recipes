dd8b6ae8abaf269629778ca54b787b63e6c68c2d
Thegaijin <semaganda@outlook.com>
2017-11-01 22:40:59 +0300

    Added routes to test the templates

--- app/views.py
+++ app/views.py
@@ -1,7 +1,21 @@
 # app/views.py
 
+# Local imports
+from app import app
 # Third party imports
 from flask import render_template
 
-# Local imports
-from app import app
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/categories')
+def categories():
+    return render_template('categories.html')
+
+
+@app.route('/ingredients')
+def ingredients():
+    return render_template('ingredients.html')

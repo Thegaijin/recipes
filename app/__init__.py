8d2b57691a5c8638b394d26968da3d7cc6cf54c4
Thegaijin <semaganda@outlook.com>
2017-11-01 22:40:11 +0300

    changes to run and  the app/__init__.py file to get around an app module error. Will come back to implement the functionality I prefer later

--- app/__init__.py
+++ app/__init__.py
@@ -2,24 +2,29 @@
 
 # Third party import
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 # local import
-from config import app_config
+# from config import app_config
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('config')
+Bootstrap(app)
 
 
-def create_app(config_name):
+''' def create_app(config_name):
     # variable app assigned an instance of class flask
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
-
+    Bootstrap(app)
     # temporary route
+
     @app.route('/')
     def hello_world():
         return 'Hello, World!'
 
-    return app
+    return app '''
 
 
 # imported at the end to avoid circular reference
 # local import
-''' from app import views '''
+from app import views

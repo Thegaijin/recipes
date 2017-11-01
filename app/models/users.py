10e1a309b3e24c7c4af5cb56303c01b6e0670c0b
Thegaijin <semaganda@outlook.com>
2017-11-01 14:26:07 +0300

    adding the css folder

--- app/models/users.py
+++ app/models/users.py
@@ -9,9 +9,12 @@ class User(object):
 
     def create_category(self, category_name):
         ''' Creates the Category 
+            Takes in one parameter, a string and adds it as a key to a 
+            dictionary with a value of an empty list to hold recipes
+
             :param category_name: A string: the name of the category to create
-            :return: The dictionary of categories, with the names as the keys 
-            and an empty list as the values
+            :return: Categories dictionary, with the names as the keys and an 
+            empty list as the values
         '''
         if not isinstance(category_name, str):
             raise TypeError('Input should be a string')
@@ -22,18 +25,40 @@ class User(object):
         return ""
 
     def view_category(self, category_name):
-        ''' Displays a Category '''
+        ''' Displays a Category
+            Takes in one parameter, a string and checks the categories 
+            dictionary for a key that matches the string.
+
+            :param category_name: A string: the name of the category to view
+            :return: The key and value pair that matches the category name 
+        '''
         viewed = {k: v for k, v in self.categories.items()
                   if category_name in k}
-        pass
+        return viewed
 
-    def edit_category(self, category_name, newname):
-        ''' Edits a Category '''
-        pass
+    def edit_category(self, category_name, new_category_name):
+        ''' Takes in two parameters, 2 strings and checks the categories 
+            dictionary for a key that matches the first string.Reassigns the 
+            value to the 2nd string as the key, deletes the first strings pair.
+
+            :param category_name: A string: the name of the category to view
+            :return: The key and value pair that matches the category name 
+        '''
+        if category_name in self.categories:
+            self.categories[new_category_name] = self.categories[category_name]
+            del self.categories[category_name]
+            return self.categories
 
     def delete_category(self, category_name):
-        ''' Deletes a Category '''
-        pass
+        ''' Takes in one parameter, checks the categories dictionary for a key 
+            that matches the first string. Deletes the key value pair.
+
+            :param category_name: A string: the name of the category to view
+            :return: The key and value pair that matches the category name 
+        '''
+        if category_name in self.categories:
+            del self.categories[category_name]
+            return self.categories
 
     def create_recipe(self, category_name, recipe_name):
         ''' Creates a recipe '''
@@ -50,3 +75,9 @@ class User(object):
     def delete_recipe(self, category_name, recipe_name):
         ''' Creates a recipe '''
         pass
+
+
+new = User()
+print(new.create_category("Cakes"))
+print(new.create_category("chapati"))
+print(new.view_category('chapati'))

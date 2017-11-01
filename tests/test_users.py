8f1db18162f6bada4aadab66739e81e4225886fc
Thegaijin <semaganda@outlook.com>
2017-11-01 12:40:59 +0300

    Added more tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -13,60 +13,123 @@ from app.models.category import Category
 
 
 @pytest.fixture(scope='module')
-def user():
+def new_user():
     ''' Returns an instance of the User class '''
     return User()
 
 
-def test_users_is_instance(user):
+@pytest.fixture(scope='module')
+def category():
+    ''' Returns a category '''
+    category_a = User().create_category('bakes')
+    return category_a
+
+
+def test_users_is_instance(new_user):
     """Test if instance of User class is successfully created"""
 
-    assert isinstance(user, User) == True
+    assert isinstance(new_user, User) is True
 
 
-def test_users_category_raise_error_if_argument_not_string(user):
+@pytest.mark.parametrize('category_name', [1, [1, 2], 100.1, -5])
+def test_users_category_error_if_argument_not_string(new_user, category_name):
     ''' Test if the parameter passed to a category is a string '''
 
     with pytest.raises(TypeError):
-        user.create_category(2347)
+        new_user.create_category('category_name')
 
 
-def test_users_category_doesnt_already_exist(user):
+def test_users_category_doesnt_already_exist(new_user):
     ''' Test if the category to be created does not already exist '''
 
-    user.create_category("cakes")
-    assert "cakes" not in user.categories
+    new_user.create_category("cakes")
+    assert "cakes" not in new_user.categories
 
 
-def test_users_category_is_created(user):
+def test_users_category_is_created(new_user, category):
     ''' Test if the category is created and added to the list '''
 
-    before_length = len(user.categories)
-    user.create_category("cakes")
-    after_length = len(user.categories)
-    x = after_length - before_length
+    pre_length = len(new_user.categories)
+    # user.create_category("cakes")
+    post_length = len(new_user.categories)
+    x = post_length - pre_length
     assert x == 1
+    assert 'cakes' in new_user.categories
 
 
-def test_users_category_is_viewed(user):
+def test_users_category_is_viewed(new_user, category):
     ''' Test if the category can be viewed '''
 
-    user.create_category("cakes")
-    assert user.view_category('cakes') == user.categories['cakes']
+    # user.create_category("cakes")
+    assert new_user.view_category('cakes') == new_user.categories['cakes']
+    # FIXME: should return a key: value pair that matches the category name
 
 
-def test_if_user_category_can_be_edited(user):
+def test_if_user_category_can_be_edited(new_user, category):
     ''' Test if the category can be edited '''
-    user.create_category("cakes")
-    user.edit_category("cakes", "bakes")
+    # user.create_category("cakes")
+    new_user.edit_category("cakes", "bakes")
     # TODO: finish this
 
 
-def test_if_user_category_can_be_deleted(user):
+def test_if_user_category_can_be_deleted(new_user, category):
     ''' Test if categories can be deleted '''
-    user.create_category("cakes")
-    before_length = len(user.categories)
-    user.delete_category("cakes")
-    after_length = len(user.categories)
-    x = before_length - after_length
+    pre_length = len(new_user.categories)
+    new_user.delete_category("cakes")
+    post_length = len(new_user.categories)
+    x = pre_length - post_length
     assert x == 1
+    assert 'cakes' not in new_user.categories
+
+#######################################################################
+#                              RECIPE TESTS                           #
+#######################################################################
+
+
+@pytest.mark.parametrize('recipe_name', [1, [1, 2], 100.1, -5])
+def test_users_recipe_error_if_not_string(new_user, category, recipe_name):
+    ''' Test if the parameter passed to a recipe is a string '''
+    with pytest.raises(TypeError):
+        new_user.create_recipe(category, 'recipe_name')
+        # TODO: finish me or delete me
+
+
+def test_users_recipe_doesnt_already_exist(new_user):
+    ''' Test if the category to be created does not already exist '''
+
+    new_user.create_recipe("cakes", "cupcakes")
+    assert "cupcakes" not in new_user.categories['cakes']
+
+
+def test_if_users_recipe_is_created(new_user, category):
+    ''' Test if the recipe is created and added to the list '''
+
+    pre_length = len(new_user.categories['cakes'])
+    new_user.create_recipe('cakes', 'cupcake')
+    post_length = len(new_user.categories['cakes'])
+    x = post_length - pre_length
+    assert x == 1
+    assert 'cupcakes' in new_user.categories['cakes']
+
+
+def test_if_users_recipes_is_viewed(new_user, category):
+    ''' Test if the recipe can be viewed '''
+    viewed = new_user.view_recipe('cakes', 'cupcakes')
+    assert 'cupcakes' in new_user.categories['cakes']
+    # FIXME: Not clear, cross check in the morning
+
+
+def test_users_edit_recipes(new_user, category):
+    ''' Test if the recipe can be edited '''
+    new_user.edit_recipe('cakes', 'cupcakes')
+    # TODO:
+
+
+def test_user_can_delete_recipe(new_user, category):
+    ''' Test if a recipe can be deleted from a category '''
+    pre_length = len(new_user.categories['cakes'])
+    new_user.delete_recipe("cakes", "cupcakes")
+    post_length = len(new_user.categories['cakes'])
+    x = pre_length - post_length
+    assert x == 1
+    assert 'cupcakes' not in new_user.categories['cakes']

0c838580d0ee23f6a75e192a0a7df23286f171d9
Thegaijin <semaganda@outlook.com>
2017-11-02 00:52:24 +0300

    Added sign in and llogout functionality

--- app/models/yummyApp.py
+++ app/models/yummyApp.py
@@ -27,6 +27,33 @@ class YummyApp(object):
                 self.users[new_user.username] = new_user
                 print("id: {}, username: {}, password: {}".format(
                     new_user.id, new_user.username, new_user.hashed_pswd))
-            return True
+                return True
             return False
         return 'User was not created'
+
+
+def signin(self, username, password):
+    """Signs in existing users.
+    Takes in 2 parameters. Check is the username exists in users.Checks if the 
+    entered password matches the credentials saved with the username
+
+    :param username: A string:
+    :param password:
+    """
+    # check if username exists
+    if username in self.users:
+        # check if password matches the value of the username key
+        hash_pswd = self.users[username].hashed_pswd
+        if check_password_hash(hash_pswd, password):
+            # return that users instance
+            return self.users[username]
+        return "The username and password combination \
+                    does not exist"
+    return "The username does not exist, please signup"
+
+    def logout(self, username):
+        """Signs out the currently logged in user
+
+        :param username: A string:
+        """
+        return "The user was logged out"

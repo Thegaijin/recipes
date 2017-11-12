1dacb6ced6e4da4c378fe0390cea9e6b61d715ad
Thegaijin <semaganda@outlook.com>
2017-11-12 15:43:03 +0300

    Refactor tests

--- tests/test_yummyapp.py
+++ tests/test_yummyapp.py
@@ -45,3 +45,114 @@ class YummpyAppTestCase(TestCase):
         self.assertEqual('password', self.the_users['username'].hashed_pswd,
                          msg='The username and password combination does not \
                         exist')
+
+#######################################################################
+#                            CATEGORY TESTS                           #
+#######################################################################
+
+    def test_yummyapp_category_creation(self):
+        ''' Test if the category is created and added to the list '''
+
+        self.the_app.signup(self.the_user)
+        self.the_app.create_category('username', "cakes", 'baked goods')
+        self.assertIn('cakes', self.the_users['username'].categories,
+                      msg='The category was not added')
+
+    def test_yummyapp_category_view(self):
+        ''' Test if the category can be viewed '''
+
+        self.the_app.signup(self.the_user)
+        self.the_app.create_category('username', "cakes", 'baked goods')
+        viewed = self.the_app.view_category('username', "cakes")
+        self.assertEqual('cakes', viewed.category_name,
+                         msg='A category by that name doesn\'t exist')
+
+    def test_yummyapp_category_name_edit(self):
+        ''' Test if the category name can be edited '''
+
+        self.the_app.signup(self.the_user)
+        self.the_app.create_category('username', "cakes", 'baked goods')
+        self.the_app.edit_category('username', "Bun", 'bun on a bun')
+        self.assertIn('Bun', self.the_users['username'].categories,
+                      msg='The category wasn\'t edited')
+
+    def test_yummyapp_category_edit(self):
+        ''' Test if the category can be edited '''
+
+        self.the_app.signup(self.the_user)
+        self.the_app.create_category('username', "cakes", 'baked goods')
+        self.the_app.edit_category('username', "cakes", 'dough on dough')
+        categories = self.the_users['username'].categories
+        self.assertEqual('dough on dough', categories['cakes'].description,
+                         msg='The category description wasn\'t edited')
+
+    def test_yummyapp_category_deletion(self):
+        ''' Test if categories can be deleted '''
+        self.the_app.signup(self.the_user)
+        self.the_app.create_category('username', "cakes", 'baked goods')
+        self.the_app.delete_category('username', "cakes")
+        self.assertNotIn('cakes', self.the_users['username'].categories,
+                         msg='The category was not deleted')
+
+#######################################################################
+#                              RECIPE TESTS                           #
+#######################################################################
+
+    def test_yummyapp_recipe_is_created(self):
+        ''' Test if the recipe is created and added to the dictionary '''
+
+        self.the_app.signup(self.the_user)
+        self.the_app.create_category('username', 'Cakes', 'baked goods')
+        self.the_app.create_recipe(
+            'username', 'Cakes', 'cupcakes', 'cakes in a cup')
+        categories = self.the_users['username'].categories
+        self.assertIn('cupcakes', categories['Cakes'].recipes)
+
+    def test_yummyapp_if_recipe_is_viewed(self):
+        ''' Test if the recipe can be viewed '''
+
+        self.the_app.signup(self.the_user)
+        self.the_app.create_category('username', 'Cakes', 'baked goods')
+        self.the_app.create_recipe(
+            'username', 'Cakes', 'cupcakes', 'cakes in a cup')
+        viewed_recipe = self.the_app.view_recipe(
+            'username', 'Cakes', 'cupcakes')
+        self.assertEqual(
+            'cupcakes', viewed_recipe)
+
+    def test_yummyapp_recipes_are_viewed(self):
+        ''' Test if all the recipes can be viewed '''
+
+        self.the_app.signup(self.the_user)
+        self.the_app.create_category('username', 'Cakes', 'baked goods')
+        self.the_app.create_recipe(
+            'username', 'Cakes', 'cupcakes', 'cakes in a cup')
+        self.the_app.create_recipe('username', 'Cakes', 'buns', 'bun bun')
+        viewed_recipes = self.the_app.view_recipes('username', 'Cakes')
+        result_len = len(viewed_recipes)
+        self.assertEqual(2, result_len)
+
+    def test_yummyapp_recipe_can_edit_recipe(self):
+        ''' Test if the recipe can be edited '''
+
+        self.the_app.signup(self.the_user)
+        self.the_app.create_category('username', 'Cakes', 'baked goods')
+        self.the_app.create_recipe(
+            'username', 'Cakes', 'cupcakes', 'cakes in a cup')
+        self.the_app.edit_recipe(
+            'username', 'Cakes', 'cupcakes', 'dough on dough')
+        categories = self.the_users['username'].categories
+        self.assertEqual(
+            'dough on dough',
+            categories['Cakes'].recipes['cupcakes'].ingredients)
+
+    def test_yummyapp_can_delete_recipe(self):
+        ''' Test if a recipe can be deleted from a category '''
+
+        self.the_app.signup(self.the_user)
+        self.the_app.create_category('username', 'Cakes', 'baked goods')
+        self.the_app.create_recipe(
+            'username', 'Cakes', 'cupcakes', 'cakes in a cup')
+        self.the_app.delete_recipe('username', 'Cakes', 'cupcakes')
+        categories = self.the_users['username'].categories
+        self.assertNotIn('cupcakes', categories['Cakes'].recipes)

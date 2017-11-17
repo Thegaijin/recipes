2b5fbe80664b51c05553c7556b6acc5a35a712bf
Thegaijin <semaganda@outlook.com>
2017-11-17 10:16:34 +0300

    Add more tests to check view functionality

--- tests/test_views.py
+++ tests/test_views.py
@@ -3,6 +3,7 @@
 
 # third party imports
 from unittest import TestCase
+from werkzeug.security import generate_password_hash
 
 # local imports
 from app import app
@@ -17,12 +18,31 @@ class ViewsTestCase(TestCase):
     def setUp(self):
         DEBUG = False
         self.the_app = app.test_client()
-        self.the_user = User('username', 'password')
-        self.new_user = User('username', 'password')
-        with app.app_context():
-            pass
+        user = YummyApp()
+        hsh_pswd = generate_password_hash('password')
+        # self.the_user = User('username', 'password')
+        # self.new_user = User('username', 'password')
+        ''' with app.app_context():
+            pass '''
 
     def test_homepage_loads(self):
         ''' Test if the home page loads '''
-        res = register()
-        self.assertIn(b'200', res.data)
+        # res = register()
+        # self.assertIn(b'200', res.data)
+        res = self.the_app.get('/', follow_redirects=True)
+        self.assertIn(b'Sign Up', res.data)
+
+    def test_user_registration(self):
+        ''' Test if user can register '''
+        # register()
+        res = self.the_app.post('/', follow_redirects=True)
+        # print(res)
+        # self.assertIn(b'Your account has beeb created', res.data)
+
+    def test_user_login(self):
+        ''' Test if user can login '''
+        res = self.the_app.post('/login/', follow_redirects=True)
+
+    def test_user_create_category(self):
+        ''' Test if user can create a category '''
+        res = self.the_app.post('/create_category/', follow_redirects=True)
